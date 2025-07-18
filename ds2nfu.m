464be03d617b02bd8e740e3f5b2d9e7836505fff
Michelle Hirsch <mhirsch@mathworks.com>
2025-07-18 14:26:25 -0400

    Finally incorporated fix for issue #1 so that it works with manual DataAspectRatio and PlotBoxAspectRatio. Folded in Kelly Kearney's excellent plotboxpos as a local function for the implementation.

--- ds2nfu.m
+++ ds2nfu.m
@@ -55,7 +55,7 @@ function varargout = ds2nfu(varargin)
 narginchk(1,3)
 
 % Determine if axes handle is specified
-if length(varargin{1})== 1 && ishandle(varargin{1}) && strcmp(get(varargin{1},'type'),'axes')	
+if isscalar(varargin{1}) && ishandle(varargin{1}) && strcmp(get(varargin{1},'type'),'axes')	
 	hAx = varargin{1};
 	varargin = varargin(2:end);
 else
@@ -66,7 +66,7 @@ errmsg = ['Invalid input.  Coordinates must be specified as 1 four-element \n' .
 	'position vector or 2 equal length (x,y) vectors.'];
 
 % Proceed with remaining inputs
-if length(varargin)==1	% Must be 4 elt POS vector
+if isscalar(varargin)	% Must be 4 elt POS vector
 	pos = varargin{1};
 	if length(pos) ~=4
 		error(errmsg);
@@ -82,8 +82,8 @@ end
 %% Get limits
 axun = get(hAx,'Units');
 set(hAx,'Units','normalized');
-if hAx.DataAspectRatioMode == "manual"
-% Do something. 
+if hAx.DataAspectRatioMode == "manual" || hAx.PlotBoxAspectRatioMode == "manual"
+    axpos = plotboxpos(hAx);
 else
     axpos = get(hAx,'Position');
 end
@@ -121,4 +121,147 @@ end
 
 %% Restore axes units
 set(hAx,'Units',axun)
+end
+
+function pos = plotboxpos(h)
+%PLOTBOXPOS Returns the position of the plotted axis region
+%
+% pos = plotboxpos(h)
+%
+% This function returns the position of the plotted region of an axis,
+% which may differ from the actual axis position, depending on the axis
+% limits, data aspect ratio, and plot box aspect ratio.  The position is
+% returned in the same units as the those used to define the axis itself.
+% This function can only be used for a 2D plot.  
+%
+% Input variables:
+%
+%   h:      axis handle of a 2D axis (if ommitted, current axis is used).
+%
+% Output variables:
+%
+%   pos:    four-element position vector, in same units as h
+
+% Copyright 2010 Kelly Kearney
+
+% Check input
+
+if nargin < 1
+    h = gca;
+end
+
+if ~ishandle(h) || ~strcmp(get(h,'type'), 'axes')
+    error('Input must be an axis handle');
+end
+
+% Get position of axis in pixels
+
+currunit = get(h, 'units');
+axisPos  = getpixelposition(h);
+
+% Calculate box position based axis limits and aspect ratios
+
+darismanual  = strcmpi(get(h, 'DataAspectRatioMode'),    'manual');
+pbarismanual = strcmpi(get(h, 'PlotBoxAspectRatioMode'), 'manual');
+
+if ~darismanual && ~pbarismanual
+    
+    pos = axisPos;
+    
+else
+
+    xlim = get(h, 'XLim');
+    ylim = get(h, 'YLim');
+    
+    % Deal with axis limits auto-set via Inf/-Inf use
+    
+    if any(isinf([xlim ylim]))
+        hc = get(h, 'Children');
+        hc(~arrayfun( @(h) isprop(h, 'XData' ) & isprop(h, 'YData' ), hc)) = [];
+        xdata = get(hc, 'XData');
+        if iscell(xdata)
+            xdata = cellfun(@(x) x(:), xdata, 'uni', 0);
+            xdata = cat(1, xdata{:});
+        end
+        ydata = get(hc, 'YData');
+        if iscell(ydata)
+            ydata = cellfun(@(x) x(:), ydata, 'uni', 0);
+            ydata = cat(1, ydata{:});
+        end
+        isplotted = ~isinf(xdata) & ~isnan(xdata) & ...
+                    ~isinf(ydata) & ~isnan(ydata);
+        xdata = xdata(isplotted);
+        ydata = ydata(isplotted);
+        if isempty(xdata)
+            xdata = [0 1];
+        end
+        if isempty(ydata)
+            ydata = [0 1];
+        end
+        if isinf(xlim(1))
+            xlim(1) = min(xdata);
+        end
+        if isinf(xlim(2))
+            xlim(2) = max(xdata);
+        end
+        if isinf(ylim(1))
+            ylim(1) = min(ydata);
+        end
+        if isinf(ylim(2))
+            ylim(2) = max(ydata);
+        end
+    end
+
+    dx = diff(xlim);
+    dy = diff(ylim);
+    dar = get(h, 'DataAspectRatio');
+    pbar = get(h, 'PlotBoxAspectRatio');
+
+    limDarRatio = (dx/dar(1))/(dy/dar(2));
+    pbarRatio = pbar(1)/pbar(2);
+    axisRatio = axisPos(3)/axisPos(4);
+
+    if darismanual
+        if limDarRatio > axisRatio
+            pos(1) = axisPos(1);
+            pos(3) = axisPos(3);
+            pos(4) = axisPos(3)/limDarRatio;
+            pos(2) = (axisPos(4) - pos(4))/2 + axisPos(2);
+        else
+            pos(2) = axisPos(2);
+            pos(4) = axisPos(4);
+            pos(3) = axisPos(4) * limDarRatio;
+            pos(1) = (axisPos(3) - pos(3))/2 + axisPos(1);
+        end
+    elseif pbarismanual
+        if pbarRatio > axisRatio
+            pos(1) = axisPos(1);
+            pos(3) = axisPos(3);
+            pos(4) = axisPos(3)/pbarRatio;
+            pos(2) = (axisPos(4) - pos(4))/2 + axisPos(2);
+        else
+            pos(2) = axisPos(2);
+            pos(4) = axisPos(4);
+            pos(3) = axisPos(4) * pbarRatio;
+            pos(1) = (axisPos(3) - pos(3))/2 + axisPos(1);
+        end
+    end
+end
+
+% Convert plot box position to the units used by the axis
+
+hparent = get(h, 'parent');
+hfig = ancestor(hparent, 'figure'); % in case in panel or similar
+currax = get(hfig, 'currentaxes');
+
+temp = axes('Units', 'Pixels', 'Position', pos, 'Visible', 'off', 'parent', hparent);
+set(temp, 'Units', currunit);
+pos = get(temp, 'position');
+delete(temp);
+
+set(hfig, 'currentaxes', currax);
+end
+
+
+
 
